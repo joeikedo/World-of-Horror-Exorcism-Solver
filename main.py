c7f2ca0af72f92c75418bf0f134dc5b64c733499
Joe Ikedo <joeikedo@gmail.com>
2024-03-17 17:36:43 -0700

    3 permutation loop

---
+++ main.py
@@ -0,0 +1,35 @@
+def addLetterHelper(index, inputString):
+    if(index == 0):
+        return inputString + 'B'
+    else:
+        return inputString + 'C'
+
+# TODO: For fun, refactor this to be a function with variable number of characters, not just 5 hardcoded loops.
+def generateEveryPossiblePermutation():
+    permutationList = []
+    currentPermutationString = ''
+    for i in range(2):
+        currentPermutationString = addLetterHelper(i, currentPermutationString)
+        for j in range(2):
+            currentPermutationString = addLetterHelper(j, currentPermutationString)
+            for k in range(2):
+                currentPermutationString = addLetterHelper(k, currentPermutationString)
+                permutationList.append(currentPermutationString)
+                # Remove last character of string to clear for next iteration of this loop.
+                currentPermutationString = currentPermutationString[:-1] 
+
+            # Clear second (j) iteration         
+            currentPermutationString = currentPermutationString[:-1]     
+
+        # Clear outermost (i) iteration
+        currentPermutationString = ''
+    
+    return permutationList
+        
+
+def main():
+    print(generateEveryPossiblePermutation())
+
+
+if __name__ == '__main__':
+    main()
